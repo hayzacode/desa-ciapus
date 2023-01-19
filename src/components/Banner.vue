eadbe2e530406f7bde81ee0e8ecbdce9382f93cc
hayzacode <119099104+hayzacode@users.noreply.github.com>
2023-01-19 10:14:34 +0700

    first commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="banner h-[300px] flex">
+        <img src="@/assets/garuda.svg" class="w-40 opacity-30 ml-16" alt=""/>
+        <div class="ml-10 flex flex-col justify-center">
+            <p class="text-black text-2xl mb-2">Profil Desa</p>
+            <p class="text-black text-3xl">DESA CIAPUS</p>
+            <p class="text-black text-2xl">Kecamatan Ciomas, Kabupaten Bogor</p>
+        </div>
+        <div class="grid place-items-center ml-auto mr-20">
+            <iframe width="500"  height="280" src="https://www.youtube.com/embed/AIa7UoAoCfA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+            </iframe>
+          </div>
+    </div>
+</template>
+<style scoped>
+.banner{
+    background: rgb(0,27,133);
+    background: linear-gradient(80deg, rgb(1, 10, 89) 0%, #2c2c8e 40%);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,54 @@
+<script setup>
+import Header from '../components/Header.vue'
+import Banner from '../components/Banner.vue'
+import Sidebar from '../components/Sidebar.vue'
+</script>
+
+<template>
+  <Header/>
+  <Banner/>
+  <main class="flex">
+    <Sidebar/>
+    <div class="flex-1 p-10">
+
+
+      <p class="text-3xl font-semibold">VISI DAN MISI ARAH KEBIJAKAN DESA </p>
+      <p>Berdasarkan kondisi saat ini maupun tantangan peluang yang akan dihadapi pada 6 (enam) tahun kedepan, 
+      maka harus disusun sebuah agenda dan prioritas pembangunan bagi pemerintahanDesa Ciapus, agar tujuan dan sasaran yang ingin 
+      dicapai dapat terukur dan terkendali. Dengan demikian, Visi dan Misi Kepala Desa terpilih harus mengakomodir hal tersebut 
+      yang selanjutnya akan dijabarkan dalam dokumen Rencana Pembangunan Jangka Menengah Desa (RPJMDesa) Ciapus periode Tahun 
+      2020-2025, yang akan dijadikan pedoman bagi setiap Stakeholder (pemangku kepentingan/seluruh masyarakat) 
+      dalam menyusun rencana program kerja dalam melaksanakan kegiatan pemerintahan di Desa Ciapus dalam kurun waktu 2020-2025.</p>
+
+      <p class="text-2xl font-semibold my-4">VISI</p>
+      <p>Berdasarkan kondisi saat ini dan tantangan yang akan dihadapi dalam 6 tahun menng serta dengan mempertimbangkan modal dasar yang dimiliki, maka Visi pembangunan Desa Ciapus Tahun 2020-2025 adalah :
+         <p class="text-1xl  font-semibold my-4center"> “Bergotong Royong Membangun Desa Ciapus Menjadi Desa Terbaik, 
+          Maju dan Mandiri yang Berlandaskan Kejujuran, Keadilan, Kesejahteraan, 
+Berbudaya serta Berakhlak Mulia” </p>
+.</p>
+      <p class="text-2xl font-semibold my-4">MISI</p>
+      <p>Selain penyusunan Visi juga telah ditetapkan misi-misi yang memuat sesuatu pernyataan yang harus dilaksanakan oleh desa, 
+        agar tercapainya visi desa tersebut. Visi berada diatas Misi. Pernyataan Visi kemudian dijabarkan ke dalam Misi agar 
+        dapat dioperasionalkan/dikerjakan. Sebagaimana penyusunan Visi, Misi pun dalam penyusunannya menggunakan pendekatan 
+        partisipatif dan pertimbangan potensi dan kebutuhan Desa Ciapus sebagaimana proses yang dilakukan, 
+        maka Misi Desa Ciapus yaitu sebagai berikut : </p>
+        <li>Membangun pemerintah desa yang berwibawa dan pelayanan maksimal terhadap masyarakat;</li>
+        <li>Peningkatan sumber daya masyarakat melalui pelatihan dan program kemasyarakatan;</li>
+        <li>Membangun UMKM dan perekonomian Mikro lainnya yang berkerjasama dengan BUMDES;</li>
+        <li>Pemerataan pembangunan di setiap wilayah dan peningkatan peranserta RT/RW di setiap pembangunan;</li>
+        <li>Membangun kawasan desa yang bersih, hijau, rapih dan nyaman;</li>
+        <li>Mewujudkan masyarakat yang religious berakhlak mulia dan mengembangkan/membumikan sedekah untuk kemandirian masyarakat;</li>
+        <li>Pelaporan APBDes yang transparan dan akuntabel melalui Website desa dan media lainnya;</li>
+        <li>Membentuk BUMDES (Badan Usaha Milik Desa) yang berkualitas dan di kelola secara professional sehingga mendapatkan benefit dan profit untuk masyarakat Desa Ciapus;</li>
+      
+      <p class="text-3xl font-semibold my-4">Struktur Desa</p>
+      <img src="@/assets/sotk_ciapus_2023.png" alt=""/>
+    </div>
+  </main>
+</template>
+
+<style>
+  body{
+    background: #f1f1f1;
+  }
+</style>
